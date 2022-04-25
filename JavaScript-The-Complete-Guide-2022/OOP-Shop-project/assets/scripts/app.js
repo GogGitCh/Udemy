c889124a0eddb20106ffa1336aa95d6cfa2bc9c5
George Dimitrov <georgiangelovdimitrov92@gmail.com>
2022-04-25 19:18:12 +0300

    Using Object and Classes

--- JavaScript-The-Complete-Guide-2022/OOP-Shop-project/assets/scripts/app.js
+++ JavaScript-The-Complete-Guide-2022/OOP-Shop-project/assets/scripts/app.js
@@ -1,63 +1,69 @@
 class Product {
-  title = "DEFAULT";
-  imageUrl;
-  description;
-  price;
+  //   title = "DEFAULT";
+  //   imageURL;
+  //   description;
+  //   price;
 
   constructor(title, image, desc, price) {
-    title;
-    this.imageUrl = image;
+    this.title = title;
+    this.imageURL = image;
     this.description = desc;
-    price;
+    this.price = price;
   }
 }
 
-const productList = {
-  products: [
+class ProductItem {
+  constructor(product) {
+      this.product = product; 
+  }
+
+  render() {
+    const prodEl = document.createElement("li");
+    prodEl.className = "product-item";
+    prodEl.innerHTML = `
+        <div>
+          <img src="${this.product.imageURL}" alt="${this.product.title}">
+          <div class="product-item__content">
+          <h2>${this.product.title}</h2>;
+          <h3>\$${this.product.price}</h3>
+          <p>${this.product.description}</p>
+          <button>Add to cart</button>
+          </div>
+          </div>
+          `;
+    return prodEl;
+  }
+}
+
+class ProductList {
+  products = [
     new Product(
       "A Chease",
       "https://cdn.shopify.com/s/files/1/2836/2982/products/brie-recipe_678x.jpg?v=1533088694",
       "bri",
       19.99
     ),
-    {
-      title: "A Chease",
-      imageURL:
-        "https://cdn.shopify.com/s/files/1/2836/2982/products/brie-recipe_678x.jpg?v=1533088694",
-      price: 19.99,
-      description: "bri",
-    },
-    {
-      title: "A Chease",
-      imageURL:
-        "https://media.istockphoto.com/photos/carpet-texture-picture-id122366121?s=612x612",
-      price: 89.99,
-      description: 'A sellout carpet from "kilimworld',
-    },
-  ],
+    new Product(
+      "A Carpet",
+      "https://media.istockphoto.com/photos/carpet-texture-picture-id122366121?s=612x612",
+      "A sellout carpet from 'kilimworld'",
+      89.99
+    ),
+  ];
+  constructor() {}
+
   render() {
     const renderHook = document.getElementById("app");
     const prodList = document.createElement("ul");
     prodList.className = "product-list";
     for (const prod of this.products) {
-      const prodEl = document.createElement("li");
-      prodEl.className = "product-item";
-      prodList.append(prodEl);
-      prodEl.innerHTML = `
-      <div>
-        <img src="${prod.imageURL}" alt="${prod.title}">
-        <div class="product-item__content">
-        <h2>${prod.title}</h2>;
-        <h3>\$${prod.price}</h3>
-        <p>${prod.description}</p>
-        <button>Add to cart</button>
-        </div>
-        </div>
-        `;
+     const productItem = new ProductItem(prod);
+     const prodEl = productItem.render();
       prodList.append(prodEl);
     }
     renderHook.append(prodList);
-  },
-};
+  }
+}
 
+const productList = new ProductList();
 productList.render();
